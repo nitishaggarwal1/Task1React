5b4c528182e904eb094a3fe254e2fe0ca3ab4bb8
nitishaggarwal1 <nitish.agarwal1993@gmail.com>
2021-10-26 17:32:13 +1100

    UI added to buttons and tables

--- reacttask/src/components/Tables.js
+++ reacttask/src/components/Tables.js
@@ -1,23 +1,25 @@
+import styles from './Table.module.css';
+
 const Table = ({headers, data}) => { 
     return (
-        <table>
-            <thead>
-                <tr>
-                {headers.map((head, index) => (
-                    <th key={index}>{head}</th>
-                ))}
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((row, index) => (
-                    <tr key={index}>
-                        {headers.map((head, index) => (
-                            <td key={index}>{row[head]}</td>
-                        ))}
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                    {headers.map((head, index) => (
+                        <th key={index}>{head}</th>
+                    ))}
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {data.map((row, index) => (
+                        <tr key={index}>
+                            {headers.map((head, index) => (
+                                <td key={index}>{row[head]}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
     );
 
 }
